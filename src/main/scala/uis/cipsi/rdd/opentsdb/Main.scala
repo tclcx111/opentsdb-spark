753b70e8d71b0005b3b5dce54482cb3ccceb83e2
Antorweep Chakravorty <antorweep.chakravorty@uis.no>
2014-07-24 12:05:16 +0200

    PairWiseRDDs

--- src/main/scala/uis/cipsi/rdd/opentsdb/Main.scala
+++ src/main/scala/uis/cipsi/rdd/opentsdb/Main.scala
@@ -6,17 +6,20 @@ import scala.tools.nsc.io.File
 import scala.tools.nsc.io.Directory
 import org.apache.spark.SparkContext
 import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.rdd.PairRDDFunctions
+ 
 
-object TestQuery extends App {       
+object TestQuery {
 
+  def main(args: Array[String]) {
   val sparkMaster = "spark://152.94.1.168:7077" //"local"
   val zookeeperQuorum = "152.94.1.168" //"localhost"
   val zookeeperClientPort = "2181"
 
   val sparkTSDB = new SparkTSDBQuery(sparkMaster, zookeeperQuorum, zookeeperClientPort)
   
-  val startdate = "1507201414:00" //"*"
-  val enddate = "1807201408:00" //"*"
+  val startdate = "1507201414:00"
+  val enddate = "1807201408:00" 
 
   val metricName = "metric.safer.actual.temperature"
   val tagKeyValue = "tag.loc->stavanger"//, tag.date->1407201414"
@@ -38,22 +41,16 @@ object TestQuery extends App {
 
   val sc = new SparkContext(sparkConf)  
   
-  val RDD = sparkTSDB.generateRDD(metricName, tagKeyValue, startdate, enddate, sc)
-  val RDDCollect = RDD.collect()
-  //RDDCollect.foreach(println)  
-  print(RDDCollect.length)
-  
-//  val ForecastTempRDD = sparkTSDB.generateRDD("metric.safer.forecast.temperature", 
-//      "tag.loc->stavanger, tag.date->1407201414", startdate, enddate, sc)
-//      
-//  val ForecastWindDRDD = sparkTSDB.generateRDD("metric.safer.forecast.windDirection", 
-//      "tag.loc->stavanger, tag.date->1407201414", startdate, enddate, sc)
-//      
-//  val ForecastPressureRDD = sparkTSDB.generateRDD("metric.safer.forecast.pressure", 
-//      "tag.loc->stavanger, tag.date->1407201414", startdate, enddate, sc)
-//  
-//  ForecastTempRDD.++(ForecastWindDRDD).++(ForecastPressureRDD).groupBy(kv => 
-//      (kv._1)).map(kv => 
-//        (kv._1,  kv._2 match {case ArrayBuffer((k1, v1), (k2, v2), (k3, v3)) => (v1, v2, v3) } ) ).collect.foreach(println)      
+  val ForecastTempRDD = sparkTSDB.generateRDD("metric.safer.forecast.temperature", 
+      "tag.loc->stavanger, tag.date->1407201414", "*", "*", sc)
+      
+  val ForecastWindDRDD = sparkTSDB.generateRDD("metric.safer.forecast.windDirection", 
+      "tag.loc->stavanger, tag.date->1407201414", startdate, enddate, sc)
+      
+  val ForecastPressureRDD = sparkTSDB.generateRDD("metric.safer.forecast.pressure", 
+      "tag.loc->stavanger, tag.date->1407201414", startdate, enddate, sc)
+  val rdd = new PairRDDFunctions(new PairRDDFunctions(ForecastTempRDD).join(ForecastWindDRDD)).join(ForecastPressureRDD)
+  rdd.map(kv => (kv._1, (kv._2._1._1, kv._2._1._2, kv._2._2))).collect.foreach(println)
+  }
   
 }
